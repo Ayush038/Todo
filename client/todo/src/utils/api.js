cc0adecf6336488eb2afe9c538f749d954b0733a
Ayush308 <creativej308@gmail.com>
2026-01-02 16:43:40 +0530

    new commit

--- client/todo/src/utils/api.js
+++ client/todo/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.baseURL
+    baseURL: "https://todo-server-ecdf.onrender.com"
 });
 
 api.interceptors.request.use(config => {
